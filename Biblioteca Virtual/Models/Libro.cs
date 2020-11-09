1ef6c4c9fbdbf2110f7216da4f34b316a0c77eae
coclares <71026063+coclares@users.noreply.github.com>
2020-11-09 20:41:58 -0300

    Carpetas agregadas Clase agregada

---
+++ Biblioteca Virtual/Models/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Virtual.Models
+{
+    public class Libro
+    {
+        public int idLibro { get; set; }
+
+        public string nombreLibro { get; set; }
+
+
+    }
+}
