ed023692b5713592932ffa74345dafc11e47a5ec
coclares <71026063+coclares@users.noreply.github.com>
2020-12-08 19:40:00 -0300

    azure

---
+++ Biblioteca Virtual/Azure/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Biblioteca_Virtual.Azure
+{
+    internal class Libro
+    {
+        public string editorialLibro { get; internal set; }
+        public string generoLibro { get; internal set; }
+        public string autorLibro { get; internal set; }
+        public string nombreLibro { get; internal set; }
+        public int idLibro { get; internal set; }
+
+        internal void Add(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
